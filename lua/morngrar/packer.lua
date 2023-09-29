344e1f7a5c37a63fe8ccf6af291fcf9a0f29e3cf
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2023-09-29 22:18:12 +0200

    feat: Adds debugging tooling
    The c/cpp debugging depends on GDB, and the `cpptools` dap installed
    through Mason

--- lua/morngrar/packer.lua
+++ lua/morngrar/packer.lua
@@ -55,4 +55,34 @@ return require('packer').startup(function(use)
         "mfussenegger/nvim-jdtls",
         ft = { "java" },
     }
+
+
+    -- debugger client:
+    use('mfussenegger/nvim-dap')
+    use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+    use('folke/neodev.nvim')
+    require("neodev").setup({
+        library = {
+            plugins = { "nvim-dap-ui" }, -- installed opt or start plugins in packpath
+            -- you can also specify the list of plugins to make available as a workspace library
+            -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
+            types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+            enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
+            -- these settings will be used for your Neovim config directory
+            runtime = true, -- runtime path
+        },
+        setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
+        -- for your Neovim config directory, the config.library settings will be used as is
+        -- for plugin directories (root_dirs having a /lua directory), config.library.plugins will be disabled
+        -- for any other directory, config.library.enabled will be set to false
+        override = function(root_dir, options) end,
+        -- With lspconfig, Neodev will automatically setup your lua-language-server
+        -- If you disable this, then you have to set {before_init=require("neodev.lsp").before_init}
+        -- in your lsp start options
+        lspconfig = true,
+        -- much faster, but needs a recent built of lua-language-server
+        -- needs lua-language-server >= 3.6.0
+        pathStrict = true,
+
+    })
 end)
