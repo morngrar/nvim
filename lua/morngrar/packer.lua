0aa61c4b8585e535676a1be393bcbb33c6921a69
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2023-11-01 20:12:17 +0100

    chore: removes some trailing space

--- lua/morngrar/packer.lua
+++ lua/morngrar/packer.lua
@@ -77,10 +77,10 @@ return require('packer').startup(function(use)
             plugins = { "nvim-dap-ui" }, -- installed opt or start plugins in packpath
             -- you can also specify the list of plugins to make available as a workspace library
             -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
-            types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-            enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
+            types = true,    -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+            enabled = true,  -- when not enabled, neodev will not change any settings to the LSP server
             -- these settings will be used for your Neovim config directory
-            runtime = true, -- runtime path
+            runtime = true,  -- runtime path
         },
         setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
         -- for your Neovim config directory, the config.library settings will be used as is
