d914934e7e637437761dca59a405c5974f6308db
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2023-11-23 22:39:32 +0100

    wip: partially failed attempt at configuring mypy integration

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -40,6 +40,34 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+
+require("lspconfig").pylsp.setup {
+settings = {
+    pylsp = {
+    plugins = {
+        -- formatter options
+        black = { enabled = false },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = true, executable = "pylint" },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = { enabled = true },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
+    },
+    },
+},
+flags = {
+    debounce_text_changes = 200,
+},
+capabilities = capabilities,
+}
+
 lsp.setup()
 
 -- The following bit is to supress the `vim` global undefined warnings of the lua lsp
