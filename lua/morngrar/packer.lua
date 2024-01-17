90e3410f49990f48cf5a53b3485012bff4e48d9a
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2024-01-17 22:21:49 +0100

    Revert "nvim-java: Non-working config"
    Gonna go back to the semi-working state for now...
    
    This reverts commit 36d3b54641132eafba5ef0c50802af547d1763e7.

--- lua/morngrar/packer.lua
+++ lua/morngrar/packer.lua
@@ -50,29 +50,6 @@ return require('packer').startup(function(use)
         config = function() require("nvim-autopairs").setup {} end
     }
 
-    use {
-        'nvim-java/nvim-java',
-        requires = {
-            'nvim-java/lua-async-await',
-            'nvim-java/nvim-java-core',
-            'nvim-java/nvim-java-test',
-            'nvim-java/nvim-java-dap',
-            'MunifTanjim/nui.nvim',
-            'neovim/nvim-lspconfig',
-            'mfussenegger/nvim-dap',
-            {
-              'williamboman/mason.nvim',
-              opts = {
-                registries = {
-                  'github:nvim-java/mason-registry',
-                  'github:mason-org/mason-registry',
-                },
-              },
-            }
-        }
-    }
-    require("java").setup()
-
     use("fatih/vim-go")
 
     -- flutter stuff
