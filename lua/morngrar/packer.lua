e2fa69b41dc7ea934a885b9d55729ac9aad16dc7
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2024-03-27 10:46:09 +0100

    fix: Adds nvim-nio, which the lua lsp seems to require

--- lua/morngrar/packer.lua
+++ lua/morngrar/packer.lua
@@ -21,6 +21,8 @@ return require('packer').startup(function(use)
         end
     })
 
+    use('nvim-neotest/nvim-nio') -- to make lua lsp shut up on opening config
+
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
 
