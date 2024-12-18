7b8ced0b6c53e7412dd4303c54ac60514058050a
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2024-12-18 13:50:24 +0100

    feat: adds git blame binding

--- lua/morngrar/plugins/fugitive.lua
+++ lua/morngrar/plugins/fugitive.lua
@@ -1,5 +1,6 @@
 return { "tpope/vim-fugitive",
-  vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "[G]it" })
+  vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "[G]it" }),
+  vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", { desc = "Git [b]lame" }),
 }
 
 -- vim: ts=2 sts=2 sw=2 expandtab
