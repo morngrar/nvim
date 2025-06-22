87df9ac87f0b2679399124ffb690cc069a779daa
Svein-Kåre Bjørnsen <github@sbjornsen.no>
2025-06-22 14:22:41 +0200

    chore: upgrades to harpoon2

--- lua/morngrar/plugins/harpoon.lua
+++ lua/morngrar/plugins/harpoon.lua
@@ -1,23 +1,22 @@
 return {
   "theprimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    local harpoon = require("harpoon")
+    harpoon:setup()
 
-    vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: [A]dd file" })
-    vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Harpoon: [E]dit list" })
-    vim.keymap.set("n", "<S-M-h>", function()
-      ui.nav_file(1)
-    end, { desc = "Harpoon: Go to file 1" })
-    vim.keymap.set("n", "<S-M-j>", function()
-      ui.nav_file(2)
-    end, { desc = "Harpoon: Go to file 2" })
-    vim.keymap.set("n", "<S-M-k>", function()
-      ui.nav_file(3)
-    end, { desc = "Harpoon: Go to file 3" })
-    vim.keymap.set("n", "<S-M-l>", function()
-      ui.nav_file(4)
-    end, { desc = "Harpoon: Go to file 4" })
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+    vim.keymap.set("n", "<S-M-h>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<S-M-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<S-M-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<S-M-l>", function() harpoon:list():select(4) end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<S-M-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<S-M-N>", function() harpoon:list():next() end)
   end
 }
 -- vim: ts=2 sts=2 sw=2 expandtab
