1e48c33ea5945b2d4a1e2215880723f1fc66a285
Svein-Kåre Bjørnsen <github@sbjornsen.no>
2025-08-29 17:59:58 +0200

    fix: prefer file-scope for c# namespaces

--- lua/morngrar/plugins/easy-dotnet.lua
+++ lua/morngrar/plugins/easy-dotnet.lua
@@ -72,7 +72,7 @@ return {
             csproj_mappings = true,
             auto_bootstrap_namespace = {
                 --block_scoped, file_scoped
-                type = "block_scoped",
+                type = "file_scoped",
                 enabled = true,
                 use_clipboard_json = {
                     behavior = "prompt", --'auto' | 'prompt' | 'never',
