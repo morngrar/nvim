ee7fd9ef73cccea92d594a2b5c7f6a4ff31d4d51
Svein-Kåre Bjørnsen <github@sbjornsen.no>
2026-08-16 13:26:09 +0200

    fix: disable autoformatting on-save for lua

--- lua/morngrar/plugins/autoformat.lua
+++ lua/morngrar/plugins/autoformat.lua
@@ -17,7 +17,7 @@ return { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true, java = true }
+      local disable_filetypes = { c = true, cpp = true, java = true, lua = true }
       return {
         timeout_ms = 1000,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
